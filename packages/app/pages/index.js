260ac22f8433830dfb7e5e66bd13038afa1e5b75
xencodes <xencodes@icloud.com>
2022-02-05 19:53:57 +0530

    app: update front page

--- packages/app/pages/index.js
+++ packages/app/pages/index.js
@@ -1,23 +1,54 @@
-import { Box, Heading, Text, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Box>
-      <Box position={'absolute'} w='50%' h='100vh' bg='#F0DB4F' zIndex={0} />
-      <VStack justifyContent={'center'} h='100vh'>
-        <Box
-          p={2}
-          zIndex={1}
-          rounded='lg'
-          shadow={2}
-          color='white'
-          bg='blackAlpha.700'
-          textAlign={'center'}
-        >
-          <Heading>JS Royale</Heading>
-          <Text>Multiplayer JavaScript Royale</Text>
-        </Box>
-      </VStack>
-    </Box>
+    <Flex>
+      <Box w='50%' bg='#F0DB4F'>
+        <VStack h='100vh' justifyContent={'center'} color='#b19c0f'>
+          <Heading fontSize={'6xl'}>JS Royale</Heading>
+          <Text textTransform={'capitalize'}>
+            Multiplayer online battle JavaScript challenge
+          </Text>
+        </VStack>
+      </Box>
+      <Box w='50%' h='100vh'>
+        <VStack h='100vh' justifyContent={'center'}>
+          <Box>
+            <Stack
+              p={4}
+              mt={4}
+              spacing={4}
+              minW={{ base: '310px', lg: '360px' }}
+              bg='gray.50'
+              rounded='md'
+              borderWidth={'1px'}
+              borderColor={'gray.200'}
+            >
+              <Heading>Create Room</Heading>
+              <FormControl>
+                <FormLabel>Room Name</FormLabel>
+                <Input placeholder='Enter room name' />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Room Password</FormLabel>
+                <Input placeholder='Enter room password' />
+              </FormControl>
+              <Button>Create</Button>
+            </Stack>
+          </Box>
+        </VStack>
+      </Box>
+    </Flex>
   )
 }
